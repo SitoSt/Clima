4385fa7760a63b0d3f2737f7c63de59f60a5b6fb
Sito <alfonsogarrelopez@gmail.com>
2024-06-15 22:53:55 +0200

    Añadiendo Wind

---
+++ src/components/Wind.tsx
@@ -0,0 +1,53 @@
+/* eslint-disable @next/next/no-img-element */
+import styles from './css/wind.module.css'
+import { CurrentDataContext } from '@/context'
+import { useContext } from 'react'
+
+export const Wind = () => {
+    const { isCurrentReady, currentData } = useContext(CurrentDataContext)
+
+    return (
+        <div className={styles.main_container}>
+            <div className={styles.box}>
+                <h2 className={styles.head}>Viento</h2>
+                <div className={styles.body}>
+                    { !isCurrentReady ? 
+                        (
+                            <>
+                                <p>cargando</p>
+                            </>
+                        ) : (
+                            <>
+                                { !currentData ?
+                                    (
+                                        <>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <div>
+                                                <p className={styles.speed}>{currentData.wind.speed} m/s</p>
+                                                {currentData.wind.gust && 
+                                                <p className={styles.gust}>Rachas de {currentData.wind.gust} m/s</p>
+                                                }
+
+                                            </div>
+                                            <div className={styles.brujula}>
+                                                <img src="/compass.png" alt="" className={styles.cartesian} />
+                                                <img src="/arrow_brujula.svg" alt="" className={styles.arrow} style={{
+                                                    rotate: `${currentData.wind.deg}deg`
+                                                }} />
+                                            </div>
+                                        </>
+                                    )
+
+                                }
+                            </>
+                        )
+
+                    }
+                </div>
+    
+            </div>
+        </div>
+    )
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { TempForecast } from "./TempForecast";
 export { Top } from './Top';
 export { FeelsLike } from './FeelsLike';
 export { Rain } from './Rain';
+export { Wind } from './Wind';
